9f8f2f76155704c0379aa4ad637ae66fc59035a7
George <giyawe98@gmail.com>
2020-01-06 16:13:58 +0100

    Projekt neu angefangen. Jetzt mit REST Schnittstellen. Erste GET Schnittstelle implementiert

---
+++ server.js
@@ -0,0 +1,24 @@
+//lädt die env datei
+require ('dotenv').config()
+
+//holt die express und mongoose dependencies
+const express = require('express')
+const app  = express()
+const mongoose = require('mongoose')
+
+//app wird mit mongoose verbunden
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+
+//database test
+db.on('err', (err)=>console.error(err))
+db.openUri('open',()=> console.log('Connected to DB'))
+
+//Server initialisiert und verbunden
+app.listen(3000, ()=> console.log('Server started'))
+app.use(express.json())
+
+
+//Express Routen
+const UserRouten = require('./routes/User')
+app.use('/User',UserRouten)
